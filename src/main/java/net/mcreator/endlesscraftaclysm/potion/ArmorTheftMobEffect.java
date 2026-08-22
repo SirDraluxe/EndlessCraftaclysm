4c16d232b543d568f103a147c3bf71219fb603e9
SirDraluxe <admin@DESKTOP-KDEBSU2>
2026-08-22 09:08:41 +0700

    Adjust effect: Armor Theft is exclusive effect to N. Alma Sword

--- src/main/java/net/mcreator/endlesscraftaclysm/potion/ArmorTheftMobEffect.java
+++ src/main/java/net/mcreator/endlesscraftaclysm/potion/ArmorTheftMobEffect.java
@@ -2,11 +2,14 @@ package net.mcreator.endlesscraftaclysm.potion;
 
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.Identifier;
 import net.minecraft.core.registries.BuiltInRegistries;
 
+import net.mcreator.endlesscraftaclysm.procedures.ArmorTheftOnEffectActiveTickProcedure;
 import net.mcreator.endlesscraftaclysm.EndlesscraftaclysmMod;
 
 public class ArmorTheftMobEffect extends MobEffect {
@@ -14,5 +17,16 @@ public class ArmorTheftMobEffect extends MobEffect {
 		super(MobEffectCategory.BENEFICIAL, -16750900);
 		this.withSoundOnAdded(BuiltInRegistries.SOUND_EVENT.getValue(Identifier.parse("block.iron.break")));
 		this.addAttributeModifier(Attributes.ARMOR, Identifier.fromNamespaceAndPath(EndlesscraftaclysmMod.MODID, "effect.armor_theft_0"), 1.5, AttributeModifier.Operation.ADD_VALUE);
+	}
+
+	@Override
+	public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+		return true;
+	}
+
+	@Override
+	public boolean applyEffectTick(ServerLevel level, LivingEntity entity, int amplifier) {
+		ArmorTheftOnEffectActiveTickProcedure.execute(entity);
+		return super.applyEffectTick(level, entity, amplifier);
 	}
 }
